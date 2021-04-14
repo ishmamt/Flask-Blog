bbbfb1075574c2516f719d327a02688eac399f42
ishmamt <55696609+ishmamt@users.noreply.github.com>
2021-04-14 13:59:38 +0600

    Ability to add, edit and delete posts

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,16 +1,16 @@
 import secrets
 import os
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog.models import User, Post
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-@app.route('/')
 @app.route('/home')
 @app.route('/index')
+@app.route('/')
 def home():
     posts = Post.query.all()
     return render_template("home.html", posts=posts)
@@ -88,9 +88,9 @@ def save_picture(form_picture):
     return picture_fn
 
 
-@app.route('/account', methods=["GET", "POST"])
+@app.route('/<string:username>', methods=["GET", "POST"])
 @login_required
-def account():
+def account(username):
     form = UpdateAccountForm()
     if form.validate_on_submit():
         if form.picture.data:
@@ -99,13 +99,13 @@ def account():
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
-        flash(f"Account has been updated.", "success")
+        flash("Account has been updated.", "success")
         return redirect(url_for("account"))
     elif request.method == "GET":
         form.username.data = current_user.username
         form.email.data = current_user.email
     image_file = url_for('static', filename=f'profile_pic/{current_user.image_file}')
-    return render_template("account.html", title='account', image_file=image_file, form=form)
+    return render_template("account.html", title=current_user.username, image_file=image_file, form=form)
 
 
 @app.route('/post/new', methods=["GET", "POST"])
@@ -116,6 +116,43 @@ def new_post():
         post = Post(title=form.title.data, content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
-        flash(f"New post created.", "success")
+        flash("New post created.", "success")
         return redirect(url_for("home"))
-    return render_template("create_post.html", title='new post', form=form)
+    return render_template("create_post.html", title='new post', form=form, legend='Create Post')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)  # forbidden route
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("The post has been updated.", "success")
+        return redirect(url_for("post", post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template("create_post.html", title='edit', form=form, legend='Update Post')
+
+
+@app.route('/post/<int:post_id>/delete', methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)  # forbidden route
+    db.session.delete(post)
+    db.session.commit()
+    flash("The post has been deleted.", "success")
+    return redirect(url_for("home"))
